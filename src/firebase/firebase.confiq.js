21e31cd36ac4d4d798686291edffebcbb3fa9894
Salman7D <salmanrayhan753@gmail.com>
2024-04-07 23:16:42 +0600

    first commit

---
+++ src/firebase/firebase.confiq.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOdE1JaAxBsM2PGScL262FmytnXbTvv2c",
+  authDomain: "real-estate-28013.firebaseapp.com",
+  projectId: "real-estate-28013",
+  storageBucket: "real-estate-28013.appspot.com",
+  messagingSenderId: "12218164503",
+  appId: "1:12218164503:web:1f2a3b0abab0fb36da0352"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
